4a7fc32d8a97c16247056d79cd3bef14f8cd8f3a
Rajat Gupta <rajat.11605708@lpu.in>
2022-03-12 14:52:02 +0530

    URL Shortener Working

--- config.py
+++ config.py
@@ -8,14 +8,16 @@ from pymongo import MongoClient
 # FastAPI Application Configuration
 app = FastAPI()
 # Memcached Client Configuration
-MEMCACHED_HOST = os.environ.get(constant.MEMCACHED_HOST, '0.0.0.0')
+MEMCACHED_HOST = os.environ.get(constant.MEMCACHED_HOST, 'localhost')
 MEMCACHED_PORT = os.environ.get(constant.MEMCACHED_PORT, '11211')
 MEMCACHED_URL = MEMCACHED_HOST + ':' + MEMCACHED_PORT
 memcache_client = memcache.Client([MEMCACHED_URL], debug=0)     # Memcached Connection
 # MongoDB Configurations
-MONGODB_HOST = os.environ.get(constant.MONGODB_HOST, '0.0.0.0')
+MONGODB_HOST = os.environ.get(constant.MONGODB_HOST, 'localhost')
 MONGODB_PORT = os.environ.get(constant.MONGODB_PORT, '27017')
 MONGODB_URL = 'mongodb://' + MONGODB_HOST + ':' + MONGODB_PORT
 mongodb_client = MongoClient(MONGODB_URL)   # MongoDB Connection
 db = mongodb_client['infracloud_db']    # Setting Database to Use
 collection = db['url_shortner']     # Setting Collection to Use
+
+from apis import url_shortener
